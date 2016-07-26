f7d368e546aadfeced8ed0a58318989761d1daf0
Howard Fraser <howardjfraser@gmail.com>
2016-07-26 10:33:40 +1000

    Change seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Person.delete_all
 
-8.times do
+24.times do
   name = "#{FFaker::Name.first_name} #{FFaker::Name.last_name}"
   Person.create! name: name, job_title: FFaker::Job.title, bio: FFaker::Lorem.paragraph
 end
